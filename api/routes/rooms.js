6d695b2b6796f1e8e4f4b6809c6216e692eee15b
Elsen01 <elsen@gmail.com>
2023-05-24 01:06:31 +0400

    UPDATE ROOM

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,5 +1,10 @@
 import express from "express";
+import {createRoom, updateRoom, updateRoomAvailability} from '../controllers/room.js'
 const router = express.Router()
+
+router.post('/:hotelid',createRoom)
+router.put('/:id',updateRoom);
+router.put('/availability/:id',updateRoomAvailability)
 
 
 export default router
