98c1fcf18b00e7dc0914f464ee4877503c296c2e
Elsen01 <elsen@gmail.com>
2023-05-25 00:52:17 +0400

    Authorization-Verification

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,11 @@
 import express from "express";
-import {updateUser} from "../controllers/user.js";
-const router = express.Router()
+import {deleteUser, getUser, getUsers, updateUser} from "../controllers/user.js";
+import {verifyAdmin, verifyUser} from "../utils/verifyToken.js";
+const router = express.Router();
 
-router.put('/:id',updateUser)
+router.put("/:id", verifyUser, updateUser);
+router.delete("/:id", verifyUser, deleteUser);
+router.get('/:id',verifyUser,getUser);
+router.get('/',verifyAdmin,getUsers)
 
-
-export default router
+export default router;
